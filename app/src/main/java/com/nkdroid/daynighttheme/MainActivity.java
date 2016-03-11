94bbacb9e6f062469721a55d2355bd584eb3dd88
nkdroidsolutions <nkdroidsolutions@gmail.com>
2016-03-11 12:36:59 +0530

    Initial commit

---
+++ app/src/main/java/com/nkdroid/daynighttheme/MainActivity.java
@@ -0,0 +1,57 @@
+package com.nkdroid.daynighttheme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button btnAutoMode,btnNightMode,btnDayMode;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Default Night Mode as Auto Mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
+        btnAutoMode= (Button) findViewById(R.id.btnAutoMode);
+        btnNightMode= (Button) findViewById(R.id.btnNightMode);
+        btnDayMode= (Button) findViewById(R.id.btnDayMode);
+
+
+        btnAutoMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set Auto Mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
+                Intent i=new Intent(MainActivity.this,ModeActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnNightMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set Default Night Mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                Intent i=new Intent(MainActivity.this,ModeActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnDayMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set Default Day Mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                Intent i = new Intent(MainActivity.this, ModeActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
